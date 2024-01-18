a5578e995dba9fae28104609780d7973a4bfa19b
tmaxkny <tmaxkny@gmail.com>
2024-01-18 18:57:42 +0900

    feat: 추가

--- focus_sr/src/page/Home/Inquiry.tsx
+++ focus_sr/src/page/Home/Inquiry.tsx
@@ -1,106 +1,138 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
 
-const Inquiry: React.FC<any> = React.forwardRef<any, any>(({ className, children }, ref) => {
+const Inquiry: React.FC<any> = React.forwardRef<any, any>(
+  ({ className, children }, ref) => {
     const CONTENTS = [
-        { title: '평균 응답 시간', type: 'FIRST', numbers: [1] },
-        { title: '문의사항 해결율', type: 'SECOND', numbers: [78] },
-        { title: '새로 들어온 문의', type: 'THIRD', numbers: [22, 13] },
-        { title: '처리 중인 문의', type: 'THIRD', numbers: [22, 13] },
+      { title: "평균 응답 시간", type: "FIRST", numbers: [1] },
+      { title: "문의사항 해결율", type: "SECOND", numbers: [78] },
+      { title: "새로 들어온 문의", type: "THIRD", numbers: [22, 13] },
+      { title: "처리 중인 문의", type: "THIRD", numbers: [22, 13] },
     ];
     return (
-        <Stack direction="row" sx={{ gap: '16px' }}>
-            {CONTENTS.map(content => (
-                <Stack
-                    key={content.title}
-                    sx={{
-                        flex: 1,
-                        height: '144px',
-                        padding: '24px',
-                        backgroundColor: '#F9FAFB',
-                        borderRadius: '12px',
-                        gap: '8px',
-                    }}
+      <Stack direction="row" sx={{ gap: "16px" }}>
+        {CONTENTS.map((content) => (
+          <Stack
+            key={content.title}
+            sx={{
+              flex: 1,
+              height: "144px",
+              padding: "24px",
+              backgroundColor: "#F9FAFB",
+              borderRadius: "12px",
+              gap: "8px",
+            }}
+          >
+            <Typography
+              sx={{ fontSize: "14px", fontWeight: "600", color: "#111928" }}
+            >
+              {content.title}
+            </Typography>
+            {content.type === "FIRST" && (
+              <Stack direction="row" alignItems="center">
+                <Typography
+                  sx={{
+                    color: "#1C64F2",
+                    fontWeight: "700",
+                    fontSize: "24px",
+                    marginRight: "4px",
+                  }}
                 >
-                    <Typography sx={{ fontSize: '14px', fontWeight: '600', color: '#111928' }}>
-                        {content.title}
-                    </Typography>
-                    {content.type === 'FIRST' && (
-                        <Stack direction="row" alignItems="center">
-                            <Typography
-                                sx={{ color: '#1C64F2', fontWeight: '700', fontSize: '24px', marginRight: '4px' }}
-                            >
-                                {content.numbers[0]}
-                            </Typography>
-                            <Typography sx={{ fontSize: '14px', fontWeight: '500', color: '#4B5563' }}>시간</Typography>
-                        </Stack>
-                    )}
-                    {content.type === 'SECOND' && (
-                        <Typography sx={{ color: '#1C64F2', fontWeight: '700', fontSize: '24px', marginRight: '4px' }}>
-                            {content.numbers[0]}%
-                        </Typography>
-                    )}
-                    {content.type === 'THIRD' && (
-                        <Stack>
-                            <Stack direction="row" alignItems="center">
-                                <Typography
-                                    sx={{
-                                        width: '90px',
-                                        marginRight: '18px',
-                                        fontSize: '14px',
-                                        fontWeight: '500',
-                                        color: '#4B5563',
-                                    }}
-                                >
-                                    기술문의
-                                </Typography>
-                                <Typography
-                                    sx={{
-                                        color: '#1C64F2',
-                                        fontWeight: '700',
-                                        fontSize: '24px',
-                                        marginRight: '4px',
-                                    }}
-                                >
-                                    {content.numbers[0]}
-                                </Typography>
-                                <Typography sx={{ fontSize: '14px', fontWeight: '500', color: '#4B5563' }}>
-                                    건
-                                </Typography>
-                            </Stack>
-                            <Stack direction="row" alignItems="center">
-                                <Typography
-                                    sx={{
-                                        width: '90px',
-                                        marginRight: '18px',
-                                        fontSize: '14px',
-                                        fontWeight: '500',
-                                        color: '#4B5563',
-                                    }}
-                                >
-                                    일반/영업문의
-                                </Typography>
-                                <Typography
-                                    sx={{
-                                        color: '#1C64F2',
-                                        fontWeight: '700',
-                                        fontSize: '24px',
-                                        marginRight: '4px',
-                                    }}
-                                >
-                                    {content.numbers[1]}
-                                </Typography>
-                                <Typography sx={{ fontSize: '14px', fontWeight: '500', color: '#4B5563' }}>
-                                    건
-                                </Typography>
-                            </Stack>
-                        </Stack>
-                    )}
+                  {content.numbers[0]}
+                </Typography>
+                <Typography
+                  sx={{ fontSize: "14px", fontWeight: "500", color: "#4B5563" }}
+                >
+                  시간
+                </Typography>
+              </Stack>
+            )}
+            {content.type === "SECOND" && (
+              <Typography
+                sx={{
+                  color: "#1C64F2",
+                  fontWeight: "700",
+                  fontSize: "24px",
+                  marginRight: "4px",
+                }}
+              >
+                {content.numbers[0]}%
+              </Typography>
+            )}
+            {content.type === "THIRD" && (
+              <Stack>
+                <Stack direction="row" alignItems="center">
+                  <Typography
+                    sx={{
+                      width: "90px",
+                      marginRight: "18px",
+                      fontSize: "14px",
+                      fontWeight: "500",
+                      color: "#4B5563",
+                    }}
+                  >
+                    기술문의
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#1C64F2",
+                      fontWeight: "700",
+                      fontSize: "24px",
+                      marginRight: "4px",
+                    }}
+                  >
+                    {content.numbers[0]}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontSize: "14px",
+                      fontWeight: "500",
+                      color: "#4B5563",
+                    }}
+                  >
+                    건
+                  </Typography>
                 </Stack>
-            ))}
-        </Stack>
+                <Stack direction="row" alignItems="center">
+                  <Typography
+                    sx={{
+                      width: "90px",
+                      marginRight: "18px",
+                      fontSize: "14px",
+                      fontWeight: "500",
+                      color: "#4B5563",
+                    }}
+                  >
+                    일반/영업문의
+                  </Typography>
+                  <Typography
+                    sx={{
+                      color: "#1C64F2",
+                      fontWeight: "700",
+                      fontSize: "24px",
+                      marginRight: "4px",
+                    }}
+                  >
+                    {content.numbers[1]}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontSize: "14px",
+                      fontWeight: "500",
+                      color: "#4B5563",
+                    }}
+                  >
+                    건
+                  </Typography>
+                </Stack>
+              </Stack>
+            )}
+          </Stack>
+        ))}
+      </Stack>
     );
-});
+  }
+);
 
 export default Inquiry;
--- focus_sr/src/page/Home/SalesDetail.tsx
+++ focus_sr/src/page/Home/SalesDetail.tsx
@@ -1,151 +1,230 @@
-import * as React from 'react';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import * as MUI_ICON from '@mui/icons-material';
-import IconButton from '@mui/material/IconButton';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
+import * as React from "react";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import * as MUI_ICON from "@mui/icons-material";
+import IconButton from "@mui/material/IconButton";
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
 
-const SalesDetail: React.FC<any> = React.forwardRef<any, any>(({ className, children }, ref) => {
+const SalesDetail: React.FC<any> = React.forwardRef<any, any>(
+  ({ className, children }, ref) => {
     const SALES_RECORDS = [
-        {
-            title: '오늘',
-            date: '2023.11.02',
-            revenue: 5500000,
-            comparedTo: '전일',
-            changedPercentage: 26.15,
-            changedAmount: 1140000,
-            new: 100,
-            renew: 139,
-            payment: 345,
-        },
-        {
-            title: '이번 주',
-            date: '2023.10.30-2023.11.05',
-            revenue: 22000000,
-            comparedTo: '전주',
-            changedPercentage: 10.55,
-            changedAmount: 2100000,
-            new: 100,
-            renew: 139,
-            payment: 345,
-        },
-        {
-            title: '이번 달',
-            date: '2023.11',
-            revenue: 12000000,
-            comparedTo: '전월',
-            changedPercentage: 50,
-            changedAmount: 4000000,
-            new: 100,
-            renew: 139,
-            payment: 345,
-        },
+      {
+        title: "오늘",
+        date: "2023.11.02",
+        revenue: 5500000,
+        comparedTo: "전일",
+        changedPercentage: 26.15,
+        changedAmount: 1140000,
+        new: 100,
+        renew: 139,
+        payment: 345,
+      },
+      {
+        title: "이번 주",
+        date: "2023.10.30-2023.11.05",
+        revenue: 22000000,
+        comparedTo: "전주",
+        changedPercentage: 10.55,
+        changedAmount: 2100000,
+        new: 100,
+        renew: 139,
+        payment: 345,
+      },
+      {
+        title: "이번 달",
+        date: "2023.11",
+        revenue: 12000000,
+        comparedTo: "전월",
+        changedPercentage: 50,
+        changedAmount: 4000000,
+        new: 100,
+        renew: 139,
+        payment: 345,
+      },
     ];
 
     return (
-        <Stack>
-            <Stack direction="row" alignItems="center" sx={{ height: '62px', marginBottom: '24px' }}>
-                <Typography
-                    sx={{
-                        color: '#4B5563',
-                        fontSize: '18px',
-                        fontWeight: '600',
-                        lineHeight: '24px',
-                    }}
-                >
-                    매출 상세
-                </Typography>
-                <IconButton disableRipple disableFocusRipple>
-                    <MUI_ICON.Refresh
-                        sx={{ color: '#6B7280', fontSize: '16px', fontWeight: '500', lineHeight: '18px' }}
-                    />
-                    <Typography sx={{ color: '#6B7280', fontSize: '12px', fontWeight: '500', lineHeight: '18px' }}>
-                        새로고침
-                    </Typography>
-                </IconButton>
-            </Stack>
-            <Stack direction="row" sx={{ gap: '16px' }}>
-                {SALES_RECORDS.map(sales => (
-                    <Stack key={sales.title} sx={{ flex: 1, alignItems: 'center' }}>
-                        <Stack
-                            sx={{
-                                alignItems: 'center',
-                                backgroundColor: '#F9FAFB',
-                                py: '24px',
-                                borderRadius: '12px',
-                                flex: 1,
-                                width: '100%',
-                            }}
-                        >
-                            <Typography
-                                sx={{
-                                    backgroundColor: '#E5E7EB',
-                                    borderRadius: '16px',
-                                    padding: '4px 8px',
-                                    marginBottom: '10px',
-                                    color: '#111928',
-                                    fontSize: '14px',
-                                    fontWeight: '500',
-                                }}
-                            >
-                                {sales.title}
-                            </Typography>
-                            <Typography
-                                sx={{ color: '#6B7280', fontSize: '14px', lineHeight: '20px', marginBottom: '10px' }}
-                            >
-                                {sales.date}
-                            </Typography>
-                            <Typography
-                                sx={{
-                                    color: '#111928',
-                                    fontSize: '20px',
-                                    lineHeight: '26px',
-                                    fontWeight: '600',
-                                    marginBottom: '8px',
-                                }}
-                            >
-                                매출액 {sales.revenue.toLocaleString()}원
-                            </Typography>
-                            <Stack direction="row" alignItems="center" sx={{ gap: '4px' }}>
-                                <Select
-                                    value={sales.comparedTo}
-                                    sx={{ height: '32px', color: '#111928', fontSize: '14px' }}
-                                >
-                                    <MenuItem value={sales.comparedTo}>{sales.comparedTo}</MenuItem>
-                                </Select>
-                                <Typography sx={{ color: '#111928', fontSize: '14px', lineHeight: '20px' }}>
-                                    대비
-                                </Typography>
-                                <Stack
-                                    direction="row"
-                                    sx={{ color: '#E02424', fontWeight: '500', fontSize: '14px', lineHeight: '20px' }}
-                                >
-                                    {sales.changedPercentage}% ({sales.changedAmount.toLocaleString()}원)
-                                    <MUI_ICON.ArrowDropUp />
-                                </Stack>
-                            </Stack>
-                        </Stack>
-                        <Stack direction="row" alignItems="center" sx={{ marginTop: '16px' }}>
-                            <Typography sx={{ marginRight: '166px' }}>신규</Typography>
-                            <Typography sx={{ color: '#1C64F2', fontWeight: '600' }}>{sales.new}</Typography>
-                            <Typography sx={{ fontSize: '16px', lineHeight: '26px', fontWeight: '700' }}>건</Typography>
-                        </Stack>
-                        <Stack direction="row" alignItems="center" sx={{ marginTop: '8px' }}>
-                            <Typography sx={{ marginRight: '166px' }}>갱신</Typography>
-                            <Typography sx={{ color: '#1C64F2', fontWeight: '600' }}>{sales.renew}</Typography>
-                            <Typography sx={{ fontSize: '16px', lineHeight: '26px', fontWeight: '700' }}>건</Typography>
-                        </Stack>
-                        <Stack direction="row" alignItems="center" sx={{ marginTop: '8px' }}>
-                            <Typography sx={{ marginRight: '166px' }}>결제</Typography>
-                            <Typography sx={{ color: '#1C64F2', fontWeight: '600' }}>{sales.payment}</Typography>
-                            <Typography sx={{ fontSize: '16px', lineHeight: '26px', fontWeight: '700' }}>건</Typography>
-                        </Stack>
-                    </Stack>
-                ))}
-            </Stack>
+      <Stack>
+        <Stack
+          direction="row"
+          alignItems="center"
+          sx={{ height: "62px", marginBottom: "24px" }}
+        >
+          <Typography
+            sx={{
+              color: "#4B5563",
+              fontSize: "18px",
+              fontWeight: "600",
+              lineHeight: "24px",
+            }}
+          >
+            매출 상세
+          </Typography>
+          <IconButton disableRipple disableFocusRipple>
+            <MUI_ICON.Refresh
+              sx={{
+                color: "#6B7280",
+                fontSize: "16px",
+                fontWeight: "500",
+                lineHeight: "18px",
+              }}
+            />
+            <Typography
+              sx={{
+                color: "#6B7280",
+                fontSize: "12px",
+                fontWeight: "500",
+                lineHeight: "18px",
+              }}
+            >
+              새로고침
+            </Typography>
+          </IconButton>
         </Stack>
+        <Stack direction="row" sx={{ gap: "16px" }}>
+          {SALES_RECORDS.map((sales) => (
+            <Stack key={sales.title} sx={{ flex: 1, alignItems: "center" }}>
+              <Stack
+                sx={{
+                  alignItems: "center",
+                  backgroundColor: "#F9FAFB",
+                  py: "24px",
+                  borderRadius: "12px",
+                  flex: 1,
+                  width: "100%",
+                }}
+              >
+                <Typography
+                  sx={{
+                    backgroundColor: "#E5E7EB",
+                    borderRadius: "16px",
+                    padding: "4px 8px",
+                    marginBottom: "10px",
+                    color: "#111928",
+                    fontSize: "14px",
+                    fontWeight: "500",
+                  }}
+                >
+                  {sales.title}
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "#6B7280",
+                    fontSize: "14px",
+                    lineHeight: "20px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  {sales.date}
+                </Typography>
+                <Typography
+                  sx={{
+                    color: "#111928",
+                    fontSize: "20px",
+                    lineHeight: "26px",
+                    fontWeight: "600",
+                    marginBottom: "8px",
+                  }}
+                >
+                  매출액 {sales.revenue.toLocaleString()}원
+                </Typography>
+                <Stack direction="row" alignItems="center" sx={{ gap: "4px" }}>
+                  <Select
+                    value={sales.comparedTo}
+                    sx={{ height: "32px", color: "#111928", fontSize: "14px" }}
+                  >
+                    <MenuItem value={sales.comparedTo}>
+                      {sales.comparedTo}
+                    </MenuItem>
+                  </Select>
+                  <Typography
+                    sx={{
+                      color: "#111928",
+                      fontSize: "14px",
+                      lineHeight: "20px",
+                    }}
+                  >
+                    대비
+                  </Typography>
+                  <Stack
+                    direction="row"
+                    sx={{
+                      color: "#E02424",
+                      fontWeight: "500",
+                      fontSize: "14px",
+                      lineHeight: "20px",
+                    }}
+                  >
+                    {sales.changedPercentage}% (
+                    {sales.changedAmount.toLocaleString()}원)
+                    <MUI_ICON.ArrowDropUp />
+                  </Stack>
+                </Stack>
+              </Stack>
+              <Stack
+                direction="row"
+                alignItems="center"
+                sx={{ marginTop: "16px" }}
+              >
+                <Typography sx={{ marginRight: "166px" }}>신규</Typography>
+                <Typography sx={{ color: "#1C64F2", fontWeight: "600" }}>
+                  {sales.new}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "16px",
+                    lineHeight: "26px",
+                    fontWeight: "700",
+                  }}
+                >
+                  건
+                </Typography>
+              </Stack>
+              <Stack
+                direction="row"
+                alignItems="center"
+                sx={{ marginTop: "8px" }}
+              >
+                <Typography sx={{ marginRight: "166px" }}>갱신</Typography>
+                <Typography sx={{ color: "#1C64F2", fontWeight: "600" }}>
+                  {sales.renew}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "16px",
+                    lineHeight: "26px",
+                    fontWeight: "700",
+                  }}
+                >
+                  건
+                </Typography>
+              </Stack>
+              <Stack
+                direction="row"
+                alignItems="center"
+                sx={{ marginTop: "8px" }}
+              >
+                <Typography sx={{ marginRight: "166px" }}>결제</Typography>
+                <Typography sx={{ color: "#1C64F2", fontWeight: "600" }}>
+                  {sales.payment}
+                </Typography>
+                <Typography
+                  sx={{
+                    fontSize: "16px",
+                    lineHeight: "26px",
+                    fontWeight: "700",
+                  }}
+                >
+                  건
+                </Typography>
+              </Stack>
+            </Stack>
+          ))}
+        </Stack>
+      </Stack>
     );
-});
+  }
+);
 
 export default SalesDetail;
